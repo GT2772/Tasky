afebf35c20be3efd8b95e97553ab23b61b310fcb
GT2772 <georgethomas19tu@gmail.com>
2021-07-29 19:34:07 +0530

    first commit

---
+++ my.js
@@ -0,0 +1,7 @@
+const getData = async () => {
+  const result = await fetch ("https://jsonplaceholder.typicode.com/users");
+  const data = await result.json();
+  console.log(data);
+};
+
+getData();
