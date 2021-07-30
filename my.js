000b83cf37a80a87037bef11698ccbccbf5d632c
GT2772 <86016479+GT2772@users.noreply.github.com>
2021-07-30 19:54:40 +0530

    Add files via upload

--- my.js
+++ my.js
@@ -1,7 +1,31 @@
-const getData = async () => {
-  const result = await fetch ("https://jsonplaceholder.typicode.com/users");
-  const data = await result.json();
-  console.log(data);
-};
+const taskContainer = document.querySelector(".task__container");
+console.log(taskContainer);
+const saveChanges = () => {
+  const taskData = {
+    id: `${Date.now()}`,
+    imageUrl: document.getElementById("imageurl").value,
+    taskTitle: document.getElementById("tasktitle").value,
+    taskType: document.getElementById("tasktype").value,
+    taskDescription: document.getElementById("taskdescription").value
+  };
 
-getData();
+const newCard = `
+<div class="col-sm-12 col-md-6 col-lg-4" id=${taskData.id}>
+<div class="card">
+<div class="card-header d-flex justify-content-end gap-2">
+ <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
+<button type="button" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+</div>
+<div class="card-body">
+<img class="card-img-top" src=${taskData.imageUrl} alt="...">
+ <h5 class="card-title mt-3 fw-bolder text-primary">${taskData.taskTitle}</h5>
+ <p class="card-text">${taskData.taskDescription}</p>
+ <a href="#" class="btn btn-primary">${taskData.taskType}</a>
+</div>
+</div>
+</div>
+`;
+
+taskContainer.insertAdjacentHTML("beforeend", newCard);
+
+};
